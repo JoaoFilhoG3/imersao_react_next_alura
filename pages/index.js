be769ae24074971086f02a04beaca8c29405e544
João Filho <joao.filho.g3@gmail.com>
2021-01-25 22:19:30 -0300

    Commit inicial do projeto

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+function Titulo(props) {
+  return (
+    <h1>
+      {props.children}
+    </h1>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Titulo>My page</Titulo>
 }
